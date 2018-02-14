27eed572a899898e8a90a5e6acb134789512ddc1
Aram <nardkf92@naver.com>
2018-02-14 17:01:40 +0900

    Initial commit

---
+++ server/edume-server.js
@@ -0,0 +1,69 @@
+// --------------------------------------------------------
+// 에듀미 애플리케이션의 웹 서버
+// --------------------------------------------------------
+import express from 'express'
+
+import bodyParser from 'body-parser'
+import morgan from 'morgan'
+import session from 'express-session'
+import mongoose from 'mongoose'
+import api from './routes'
+import database from './database/database'
+
+import webpack from 'webpack';
+import WebpackDevServer from 'webpack-dev-server';
+const config = require('../../../Downloads/Start3/server/config/config');
+const webConfig = require('../webpack.dev.config');
+const path = require('path');
+const http = require('http');
+
+var cookieParser = require('cookie-parser');
+// 서버 실행
+const app = express()
+/////////////////////////////////////////////////////////////
+/* HTTP 요청을 로그하는 미들웨어: morgan */
+app.use(morgan('dev'))
+
+/////////////////////////////////////////////////////////////
+app.use(bodyParser.urlencoded({ entended : true }));
+app.use(bodyParser.json());
+///////////////////////////////////////////
+app.use(cookieParser());//쿠키설정
+
+app.use(session({//세션설정
+    secret:'edu_key',
+    resave:true,
+    saveUninitialized:true
+}));
+
+app.use('/', express.static(path.join(__dirname, './../public')));
+
+app.use('/api', api)
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './../public/index.html'));
+});
+///////////////////////////////////////////
+/* handle error */
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// app.use('/', router);//라우터 객체 등록
+
+app.listen(config.server_port, () => {
+    console.log('서버 실행 완료:', `http://localhost:` + config.server_port)
+    database.init(app,config);
+})
+
+if(process.env.NODE_ENV == 'development') {
+    console.log('Server is running on development mode');
+    const compiler = webpack(webConfig);
+    const devServer = new WebpackDevServer(compiler, webConfig.devServer);
+    devServer.listen(
+        webConfig.devServer_port, () => {
+            console.log('webpack-dev-server is listening on port', webConfig.devServer_port);
+        }
+    );
+}
---
+++ src/containers/Chat.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Chat extends React.Component{
+
+
+    render() {
+        return (
+            <div>
+                채팅창 구현할끄아!
+            </div>
+        );
+    }
+}
+
+
+export default Chat;
