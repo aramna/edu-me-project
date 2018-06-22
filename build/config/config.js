3ab33a325e56b91e45eba631a78791c9785305ae
Aram <nardkf92@naver.com>
2018-06-22 09:14:21 +0900

    '131415'

--- build/config/config.js
+++ build/config/config.js
@@ -2,8 +2,7 @@
 
 module.exports = {
     server_port: 3000,
-    devServer_port: 3000,
-
+    devServer_port: 4000,
     db_url: 'mongodb://dydcks5:c1w1s1c1!!@ds018168.mlab.com:18168/oz',
     db_schemas: [{ file: './schema/user_schema', collection: 'Users', schemaName: 'UserSchema', modelName: 'UserModel' }, { file: './schema/chat_schema', collection: 'Chats', schemaName: 'ChatSchema', modelName: 'ChatModel' }, { file: './schema/room_schema', collection: 'Room', schemaName: 'RoomSchema', modelName: 'RoomModel' }, { file: './schema/list_schema', collection: 'List', schemaName: 'ListSchema', modelName: 'ListModel' }, { file: './schema/bot_schema', collection: 'Bot', schemaName: 'BotSchema', modelName: 'BotModel' }],
     route_info: []
